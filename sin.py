16b0e8d4e359ab6da89984ccc30f4cfac5968096
nur <nurterzi1925@icloud.com>
2023-12-27 09:52:16 +0300

    Update sin.py

--- sin.py
+++ sin.py
@@ -8,7 +8,7 @@ for x in range(1, 21):
 
 import math
 
-sayi = 10  # İstediğiniz sayıyı buraya yazın
+sayi = 10  
 sinus_degeri = math.sin(math.radians(sayi))
 
 print(f"Sinüs({sayi}): {sinus_degeri}")
